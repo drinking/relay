d9cbb15419a6a13885ccddd7dc71356f57624cd5
panjunqing <panjunqing@ke.com>
2024-04-19 21:59:58 +0800

    First commit

--- app/acceptor/page.tsx
+++ app/acceptor/page.tsx
@@ -1,34 +1,35 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from 'next/navigation'
+import React, { useState, useEffect, Fragment } from "react";
 
 export default function Home() {
   const router = useRouter()
+
+  const [NO, setNO] = useState(1);
+
+  useEffect(() => {
+
+  }, [])
+
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <Image
-        src='/static/bg1.jpg'
+        src='/static/bg2.jpg'
         alt="Background Image"
         layout="fill"
         objectFit="cover"
         quality={100}
       />
 
-      {/* <div className="absolute">
-        做规范使用
-        电动自行车的践行者
-        倡导者和传播者！
-      </div> */}
-
-      <button className="text-white-900 absolute" onClick={() => {
-        router.push('/acceptor')
-      }}>
-        我承诺
-      </button>
+      <div className="absolute text-gray-900">
+        您是第{NO}位承诺人
+      </div>
 
 
-      <div>Copyright@ 2018 Jiaye Co.Ltd.沪ICP备18003803号-1</div>
 
     </main>
   );
