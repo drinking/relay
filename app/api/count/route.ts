fdf773eef8de8d25bca7f4bd0a3403336063a935
panjunqing <panjunqing@ke.com>
2024-04-20 23:43:12 +0800

    Update

--- app/api/count/route.ts
+++ app/api/count/route.ts
@@ -2,77 +2,77 @@
 import { Database, RunResult } from 'sqlite3';
 
 // Open the database connection
-const db = new Database('/relay/relay.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log('Connected to the SQLite database.');
-    }
-});
+// const db = new Database('/relay/relay.db', (err) => {
+//     if (err) {
+//         console.error(err.message);
+//     } else {
+//         console.log('Connected to the SQLite database.');
+//     }
+// });
 
-db.run(`
-  CREATE TABLE IF NOT EXISTS requests (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    ip TEXT,
-    count INTEGER DEFAULT 0
-  )
-`);
+// db.run(`
+//   CREATE TABLE IF NOT EXISTS requests (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     ip TEXT,
+//     count INTEGER DEFAULT 0
+//   )
+// `);
 
-interface IPAddress {
-    ip: string;
-}
+// interface IPAddress {
+//     ip: string;
+// }
 
-const logIP = (ip: string, db: Database): Promise<RunResult> => {
-    return new Promise((resolve, reject) => {
-        db.run(
-            'INSERT INTO requests (ip) VALUES (?)',
-            [ip],
-            function (err: Error | null) {
-                console.log('Inserted new row', err);
-                if (err) {
-                    console.error(err.message);
-                    reject(err);
-                } else {
-                    resolve(this);
-                }
-            }
-        );
-    });
-};
+// const logIP = (ip: string, db: Database): Promise<RunResult> => {
+//     return new Promise((resolve, reject) => {
+//         db.run(
+//             'INSERT INTO requests (ip) VALUES (?)',
+//             [ip],
+//             function (err: Error | null) {
+//                 console.log('Inserted new row', err);
+//                 if (err) {
+//                     console.error(err.message);
+//                     reject(err);
+//                 } else {
+//                     resolve(this);
+//                 }
+//             }
+//         );
+//     });
+// };
 
-const loadSeq = (): Promise<IPAddress[]> => {
-    return new Promise((resolve, reject) => {
-        db.all("SELECT DISTINCT ip from requests", [], (err, rows: IPAddress[]) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows)
-            }
-        });
-    })
-}
+// const loadSeq = (): Promise<IPAddress[]> => {
+//     return new Promise((resolve, reject) => {
+//         db.all("SELECT DISTINCT ip from requests", [], (err, rows: IPAddress[]) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(rows)
+//             }
+//         });
+//     })
+// }
 
 var NO = -1;
 const ipNumberMap: Map<string, number> = new Map<string, number>();
 
 export async function GET(request: Request) {
 
-    if (request.headers.get('x-custom-ip') && NO == -1) {
-        const rows = await loadSeq()
-        NO = rows.length;
-        for (let i = 0; i < rows.length; i++) {
-            const item = rows[i];
-            ipNumberMap.set(item.ip, i + 1);
-        }
-    }
+    // if (request.headers.get('x-custom-ip') && NO == -1) {
+    //     const rows = await loadSeq()
+    //     NO = rows.length;
+    //     for (let i = 0; i < rows.length; i++) {
+    //         const item = rows[i];
+    //         ipNumberMap.set(item.ip, i + 1);
+    //     }
+    // }
 
     const ip = request.headers.get('x-custom-ip') ?? request.headers.get('x-forwarded-for') ?? ""
 
-    try {
-        logIP(ip, db);
-    } catch (err) {
-        console.error(err);
-    }
+    // try {
+    //     logIP(ip, db);
+    // } catch (err) {
+    //     console.error(err);
+    // }
 
     if (ipNumberMap.get(ip)) {
         return Response.json({ no: ipNumberMap.get(ip) });
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,164 @@
 "use client";
-import Image from "next/image";
+import { default as NextImage } from "next/image";
 import { useRouter } from 'next/navigation'
+import React, { useEffect, useRef, useState } from 'react';
+import { useSearchParams } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter()
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div className="h2">
-        一起签署电动自行车用车安全承诺书，守护平安家园
+  const searchParams = useSearchParams()
+
+  const [imageUrl, setImageURL] = useState('/static/bg2.png');
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [canvasImageSrc, setCanvasImageSrc] = useState<string>('');
+  const [shareImage, setShareImage] = useState<boolean>(false);
+  const [NO, setNO] = useState<Number>(0);
+
+
+  useEffect(() => {
+
+
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        // Set the canvas dimensions to match the browser window
+        canvas.width = window.innerWidth * 2;
+        canvas.height = window.innerHeight * 2;
+
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Load the image
+        const img = new Image();
+        img.src = imageUrl;
+        img.onload = function () {
+
+          const imageAspectRatio = img.width / img.height;
+          const canvasAspectRatio = canvas.width / canvas.height;
+
+          let renderableWidth, renderableHeight;
+
+          // If image's aspect ratio is less than canvas's aspect ratio
+          // if (imageAspectRatio < canvasAspectRatio) {
+          //     renderableHeight = canvas.height;
+          //     renderableWidth = img.width * (renderableHeight / img.height);
+          // } else {
+          renderableWidth = canvas.width;
+          renderableHeight = img.height * (renderableWidth / img.width);
+          // }
+
+          // Calculate the position to draw the image
+          const xStart = (canvas.width - renderableWidth) / 2;
+          const yStart = 0; //(canvas.height - renderableHeight) / 2;
+
+          // Draw the image on the canvas
+          const scale = 2;
+          ctx.drawImage(img, xStart, yStart, renderableWidth, renderableHeight);
+
+
+          // Set the font and text styles
+          ctx.font = '45px Microsoft YaHei';
+          ctx.fillStyle = 'black';
+          // ctx.strokeStyle = 'black';
+          // ctx.lineWidth = 1;
+
+          const textWidth = 4 * 45 + String(NO).length * 28;
+
+          const textStart = (canvas.width - textWidth) / 2;
+          const textYStart = canvas.height / 7;
+          // Write the text on the image
+          ctx.fillText('您是第', textStart, textYStart);
+          ctx.fillStyle = 'red';
+          ctx.fillText(String(NO), textStart + 140, textYStart);
+          ctx.fillStyle = 'black';
+          ctx.fillText('位', textStart + 140 + String(NO).length * 28, textYStart);
+
+          const textStart2 = (canvas.width - 530) / 2;
+          ctx.fillText('电动自行车用车安全承诺人', textStart2, textYStart + 70);
+
+          // Convert the canvas to a data URL
+          const canvasDataUrl = canvas.toDataURL('image/png');
+          setCanvasImageSrc(canvasDataUrl);
+        }
+      }
+    }
+  }, [NO]);
+
+  const handleClick = () => {
+    fetch('/api/count').then(res => {
+      res.json().then(data => {
+        setShareImage(true)
+        setNO(data.no)
+        // router.push('/relay/acceptor?no=' + data.no)
+      })
+    })
+  };
+
+  return (
+    <>
+      <div id='wx_pic' style={{ margin: '0 auto', display: 'none' }}>
+        <img src='/static/share.jpg' />
       </div>
 
-      <div>Copyright@ 2018 Jiaye Co.Ltd.沪ICP备18003803号-1</div>
 
-    </main>
+      {!shareImage && <main className="flex min-h-screen flex-col items-center justify-between bg-[#ea5504]">
+
+        {
+          !shareImage && <NextImage
+            alt="Image Alt"
+            // src="https://static.vintage.love/images/middle/NMK4ZBMRYBBCQtsr0AsrHEmcBUeJUhCIvltjWAdp.png"
+            src='/static/part1.png'
+            className="pt-10 pl-10 pr-10"
+            width={360}
+            height={300}
+            objectFit="contain" // Scale your image down to fit into the container
+          />
+        }
+
+        {
+          !shareImage &&
+
+          <button onClick={handleClick}>
+            <NextImage
+              alt="Image Alt"
+              // src="https://static.vintage.love/images/middle/51L1ZC4lzAyZggtDl164l6UNGBv5kqq1OpEDfSQJ.png"
+              src='/static/part2.png'
+              width={200}
+              height={100}
+              objectFit="contain" // Scale your image down to fit into the container
+            />
+
+
+          </button>
+        }
+
+        {
+          !shareImage &&
+          <NextImage
+            alt="Image Alt"
+            // src="https://static.vintage.love/images/middle/GOfUUs3UMxdzMZHIflqK7uBanjHAeN0ikQdyb5KJ.png"
+            src='/static/part3.png'
+            style={{ alignSelf: 'end' }}
+            width={300}
+            height={300}
+            objectFit="contain" // Scale your image down to fit into the container
+          />
+        }
+
+      </main>
+      }
+
+      {
+        shareImage && <div className='bg-white'>
+          {!canvasImageSrc && <canvas ref={canvasRef} style={{ border: '1px solid black', transform: 'scale(0.5,0.5)', transformOrigin: '0 0' }} />}
+          {canvasImageSrc &&
+            <NextImage src={canvasImageSrc} alt="Background Image" layout="fill" objectFit="cover" quality={100} />
+          }
+        </div>
+      }
+    </>
   );
 }
