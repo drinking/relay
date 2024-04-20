ffbeca7ec1583b57b20604e117ab9bdfd53148de
panjunqing <panjunqing@ke.com>
2024-04-20 21:56:34 +0800

    Update

--- app/relay/page.tsx
+++ app/relay/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from 'next/navigation'
+import Head from 'next/head'
 
 export default function Home() {
     const router = useRouter()
@@ -14,40 +15,48 @@ export default function Home() {
     };
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between bg-[#ea5504]">
+        <>
 
-            <Image
-                alt="Image Alt"
-                // src="https://static.vintage.love/images/middle/NMK4ZBMRYBBCQtsr0AsrHEmcBUeJUhCIvltjWAdp.png"
-                src='/static/part1.png'
-                className="pt-10 pl-10 pr-10"
-                width={360}
-                height={300}
-                objectFit="contain" // Scale your image down to fit into the container
-            />
+            <div id='wx_pic' style={{ margin: '0 auto', display: 'none' }}>
+                <img src='/static/share.jpg' />
+            </div>
 
-            <button onClick={handleClick}>
+
+            <main className="flex min-h-screen flex-col items-center justify-between bg-[#ea5504]">
+
                 <Image
                     alt="Image Alt"
-                    // src="https://static.vintage.love/images/middle/51L1ZC4lzAyZggtDl164l6UNGBv5kqq1OpEDfSQJ.png"
-                    src='/static/part2.png'
-                    width={200}
-                    height={100}
+                    // src="https://static.vintage.love/images/middle/NMK4ZBMRYBBCQtsr0AsrHEmcBUeJUhCIvltjWAdp.png"
+                    src='/static/part1.png'
+                    className="pt-10 pl-10 pr-10"
+                    width={360}
+                    height={300}
                     objectFit="contain" // Scale your image down to fit into the container
                 />
 
+                <button onClick={handleClick}>
+                    <Image
+                        alt="Image Alt"
+                        // src="https://static.vintage.love/images/middle/51L1ZC4lzAyZggtDl164l6UNGBv5kqq1OpEDfSQJ.png"
+                        src='/static/part2.png'
+                        width={200}
+                        height={100}
+                        objectFit="contain" // Scale your image down to fit into the container
+                    />
 
-            </button>
 
-            <Image
-                alt="Image Alt"
-                // src="https://static.vintage.love/images/middle/GOfUUs3UMxdzMZHIflqK7uBanjHAeN0ikQdyb5KJ.png"
-                src='/static/part3.png'
-                style={{ alignSelf: 'end' }}
-                width={300}
-                height={300}
-                objectFit="contain" // Scale your image down to fit into the container
-            />
-        </main>
+                </button>
+
+                <Image
+                    alt="Image Alt"
+                    // src="https://static.vintage.love/images/middle/GOfUUs3UMxdzMZHIflqK7uBanjHAeN0ikQdyb5KJ.png"
+                    src='/static/part3.png'
+                    style={{ alignSelf: 'end' }}
+                    width={300}
+                    height={300}
+                    objectFit="contain" // Scale your image down to fit into the container
+                />
+            </main>
+        </>
     );
 }
